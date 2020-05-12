fb792460446ea2e7c08757c4aaf10fb63df764d9
Raveen K Sharma <raveen08@gmail.com>
2020-05-12 00:57:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,7 +110,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
+ 
      
 end
 
@@ -130,5 +132,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
 
 end
